c21c41d5f7581ce7d46b30bef028ecb7996f17fc
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-07-08 15:41:32 +0000

    let var scope

--- 05_scoping/01_local_global_scoping.js
+++ 05_scoping/01_local_global_scoping.js
@@ -3,13 +3,15 @@
 // var can be accessed globally which is not good
 // fields in global can be accessed in local but local fields cannot be accessed in global scoping
 
+let b = 800
 
 if(true){
     var a = 700
     let b = 200
+    console.log("inner b: ", b);
 }
 
-console.log(a); 
-// console.log(b); //gives error
+console.log("Should be inner a but is declared with var so no scoping", a); 
+console.log("outer b: ", b); 
 
 
