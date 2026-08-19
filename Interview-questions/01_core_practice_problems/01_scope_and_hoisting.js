95ad898bd871bb92b7f335d0b055d983dc318ca2
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-08-19 09:57:04 +0000

    function and block scope

--- Interview-questions/01_core_practice_problems/01_scope_and_hoisting.js
+++ Interview-questions/01_core_practice_problems/01_scope_and_hoisting.js
@@ -1,6 +1,6 @@
-// Normal hoisting with let vat and const
+// Normal hoisting with let var and const
 
-console.log(a)
+// console.log(a)
 var a = 10
 
 // console.log(b)
@@ -8,3 +8,45 @@ let b = 10
 
 // console.log(c)
 const c = 10
+
+
+// ------------------------------------------------------------------------------------------------------------
+
+
+// Function vs block scope
+
+// let var anc const
+// var - function scope
+// let & const - block scope
+
+var d = 10
+function test(){
+  // console.log(d)
+  var d = 20
+}
+test()
+
+let e = 10             // same with const as well
+function testt() {
+  // console.log(e)
+  let e = 20
+}
+testt()
+
+var f = 10
+function ttest() {
+  // console.log(f)
+}
+ttest()
+
+var g = 10
+{
+  var g = 20
+}
+// console.log(g)
+
+let h = 10
+{
+  let h = 20
+}
+console.log(h)
