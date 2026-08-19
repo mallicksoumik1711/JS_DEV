083c29279d1d80890f6ac5ae1f198b78b0ef08d3
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-08-19 09:46:54 +0000

    hoisting questions with let var and const

--- Interview-questions/01_core_practice_problems/01_scope_and_hoisting.js
+++ Interview-questions/01_core_practice_problems/01_scope_and_hoisting.js
@@ -1,74 +1,10 @@
-// console.log(a)
-// var a = 10
-// this will print undefined as var is hoisted but before declaration its printed
+// Normal hoisting with let vat and const
+
+console.log(a)
+var a = 10
 
 // console.log(b)
-// const b = 10
-// reference error - let and const are hoisted but remain at TDZ till they're assignment (=10)
+let b = 10
 
-// function test() {
-//   console.log(a);
-//   var a = 20;
-// }
-
-// test();
-
-// function test() {
-//   console.log(a);
-//   const a = 20;
-// }
-
-// test();
-
-// let a = 10
-// function test(){
-//     console.log(a)
-//     let a = 20
-// }
-// test()
-
-// let a = 10;
-// {
-//   console.log(a);
-//   let a = 20;
-// }
-
-// var a = 10;
-// {
-//   console.log(a);
-//   var a = 20;
-// }
-
-// var a = 10;
-// {
-//   var a = 20;
-// }
-// console.log(a);
-
-// let a = 10;
-// {
-//   let a = 20;
-// }
-// console.log(a);
-
-// console.log(a);
-// console.log(b);
-// var a = 10;
-// let b = 20;
-
-// console.log(a);
-// function test() {
-//     console.log(a)
-//   var a = 100;
-// }
-// var a = 10;
-// console.log(a)
-// test()
-// console.log(a)
-
-var a = 10;
-function test() {
-  console.log(a);
-  let a = 20;
-}
-test();
+// console.log(c)
+const c = 10
