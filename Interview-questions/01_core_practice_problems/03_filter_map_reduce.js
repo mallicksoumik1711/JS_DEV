de8b5aa4c525b415acd2bd651e06c587175d1c1c
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-08-23 10:17:12 +0000

    more practice questons on map filter reduce

--- Interview-questions/01_core_practice_problems/03_filter_map_reduce.js
+++ Interview-questions/01_core_practice_problems/03_filter_map_reduce.js
@@ -94,4 +94,60 @@ const users = [
 // Using filter() and map(), return the names of all active users.
 
 const activeUsers = users.filter((user) => user.active).map((user) => user.name)
-console.log(activeUsers)
+// console.log(activeUsers)
+
+
+
+const nums3 = [1, 2, 3, 4, 5];
+// How would you calculate the sum of the squares of only the even numbers?
+const sumOfSquare = nums3.filter((num) => num%2===0).map((num) => num*num).reduce((total, num) => total + num, 0);
+// console.log(sumOfSquare)
+
+
+const products = [
+    { name: "Laptop", category: "electronics", price: 1000 },
+    { name: "Phone", category: "electronics", price: 500 },
+    { name: "Shirt", category: "clothing", price: 100 },
+    { name: "Shoes", category: "clothing", price: 150 }
+];
+// Using array methods, calculate the total price of electronics only.
+const totalPrice = products.filter((product) => product.category === "electronics").reduce((acc, obj) => acc+obj.price, 0);
+// console.log(totalPrice)
+
+
+
+// Can reduce() replace both filter() and map()?
+// For example, can you solve this:
+const reduceOnly = [1, 2, 3, 4, 5, 6];
+// and produce:
+// [20, 40, 60]
+// using only reduce()?
+// If yes, write the code.
+
+const ansWReduce = reduceOnly.reduce((acc, num) => {
+    if(num % 2 == 0){
+        acc.push(num*10);
+    }
+    return acc;
+}, [])
+// console.log(ansWReduce)
+
+
+
+const employees = [
+    { name: "John", department: "IT", salary: 50000 },
+    { name: "Alice", department: "HR", salary: 45000 },
+    { name: "Bob", department: "IT", salary: 60000 },
+    { name: "Sarah", department: "Finance", salary: 55000 },
+    { name: "Mike", department: "IT", salary: 70000 }
+];
+// Using filter(), map(), and reduce(), find the average salary of IT employees.
+let count = 0;
+const onlyEmp = employees.filter((employee) => {
+    return employee.department === "IT"
+}).reduce((acc, ans) => {
+    count += 1
+    // console.log(count)
+    return acc+ans.salary
+}, 0)
+console.log(onlyEmp/count)
