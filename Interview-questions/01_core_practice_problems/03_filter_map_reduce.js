05e4b1cd7def9482bc3053879868bc32337c39eb
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-08-23 07:55:52 +0000

    mixed questions

--- Interview-questions/01_core_practice_problems/03_filter_map_reduce.js
+++ Interview-questions/01_core_practice_problems/03_filter_map_reduce.js
@@ -4,7 +4,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 // [2, 4, 6, 8]
 
 const evenNums = numbers.filter((num) => num%2==0)
-console.log(evenNums)
+// console.log(evenNums)
 
 // ---------------------------------------------------------------
 
@@ -14,14 +14,14 @@ const numbers2 = [1, 2, 3, 4, 5];
 // [1, 4, 9, 16, 25]
 
 const sqNums = numbers2.map((num) => num*num)
-console.log(sqNums)
+// console.log(sqNums)
 
 const names = ["john", "alice", "bob"];
 // Use map() to create:
 // ["JOHN", "ALICE", "BOB"]
 
 const uppercaseNames = names.map((name) => name.toUpperCase())
-console.log(uppercaseNames)
+// console.log(uppercaseNames)
 
 // ---------------------------------------------------------------
 
@@ -33,7 +33,7 @@ const numbers3 = [5, 10, 15, 20];
 const addedNums = numbers3.reduce((total, num) => {
     return total + num;
 })
-console.log(addedNums)
+// console.log(addedNums)
 
 const numbers4 = [2, 3, 4, 5];
 // Use reduce() to calculate the product of all numbers.
@@ -42,4 +42,30 @@ const numbers4 = [2, 3, 4, 5];
 
 const prodNums = numbers4.reduce((total, num) => total*num, 100)
 // last 100 sets the value of total initially
-console.log(prodNums)
+// console.log(prodNums)
+
+// ------------------------------------------------------------
+
+// COMBINED
+
+const nums = [1, 2, 3, 4, 5, 6];
+// Use filter() and map() to:
+// Keep only even numbers.
+// Multiply each remaining number by 10.
+// Expected result:
+// [20, 40, 60]
+
+const ans = nums.filter((num) => num % 2 === 0).map((num) => num*10)
+// console.log(ans)
+
+
+
+const nums2 = [5, 10, 15, 20, 25];
+// Use filter() and reduce() to:
+// Keep numbers greater than 10.
+// Calculate their total.
+// Expected result:
+// 60
+
+const ans2 = nums2.filter((num) => num > 10).reduce((total, num) => total + num);
+console.log(ans2)
