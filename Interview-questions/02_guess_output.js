d1ff0d2853180056245713b62b495383726f6c4b
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-09-19 09:53:09 +0000

    guess 02

--- Interview-questions/02_guess_output.js
+++ Interview-questions/02_guess_output.js
@@ -1,16 +1,34 @@
 // 1. Promise + setTimeout + microtask queue
-console.log("A");
+// console.log("A");
 
-setTimeout(() => {
-  console.log("B");
-}, 0);
+// setTimeout(() => {
+//   console.log("B");
+// }, 0);
 
-Promise.resolve()
-  .then(() => {
-    console.log("C");
-  })
-  .then(() => {
-    console.log("D");
-  });
+// Promise.resolve()
+//   .then(() => {
+//     console.log("C");
+//   })
+//   .then(() => {
+//     console.log("D");
+//   });
 
-console.log("E");
+// console.log("E");
+
+
+
+// 2. this + arrow function
+// const user = {
+//   name: "Soumik",
+
+//   normal() {
+//     console.log(this.name);
+//   },
+
+//   arrow: () => {
+//     console.log(this.name);
+//   }
+// };
+
+// user.normal();
+// user.arrow();
