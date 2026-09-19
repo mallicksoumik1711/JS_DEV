756e3bc133b7d35927fdd4a7291b60d7268ef279
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2026-09-19 09:43:50 +0000

    guess output questions

---
+++ Interview-questions/02_guess_output.js
@@ -0,0 +1,16 @@
+// 1. Promise + setTimeout + microtask queue
+console.log("A");
+
+setTimeout(() => {
+  console.log("B");
+}, 0);
+
+Promise.resolve()
+  .then(() => {
+    console.log("C");
+  })
+  .then(() => {
+    console.log("D");
+  });
+
+console.log("E");
